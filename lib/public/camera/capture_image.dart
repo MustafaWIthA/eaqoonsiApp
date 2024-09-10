4f7df2197aed27071f970058446126b6e4c81cc8
Mustafa <mustafa.abdsh@gmail.com>
2024-09-10 13:44:13 +0300

    Update capture_image.dart

--- lib/public/camera/capture_image.dart
+++ lib/public/camera/capture_image.dart
@@ -1,6 +1,5 @@
 import 'package:eaqoonsi/widget/app_export.dart';
 import 'package:eaqoonsi/widget/text_theme.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 final cameraControllerProvider = FutureProvider<CameraController?>((ref) async {
   final cameras = await availableCameras();
@@ -16,11 +15,10 @@ final cameraControllerProvider = FutureProvider<CameraController?>((ref) async {
   return controller;
 });
 
-class CameraScreen extends ConsumerStatefulWidget {
+class CameraScreen extends ConsumerWidget {
   final String fullName;
   final String email;
   final String password;
-
   const CameraScreen({
     required this.fullName,
     required this.email,
@@ -29,172 +27,107 @@ class CameraScreen extends ConsumerStatefulWidget {
   });
 
   @override
-  ConsumerState<CameraScreen> createState() => _CameraScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cameraControllerFuture = ref.watch(cameraControllerProvider.future);
 
-class _CameraScreenState extends ConsumerState<CameraScreen> {
-  late Future<void> _cameraPermissionFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    _cameraPermissionFuture = _requestCameraPermission();
-  }
-
-  Future<void> _requestCameraPermission() async {
-    final status = await Permission.camera.request();
-    if (status != PermissionStatus.granted) {
-      throw Exception('Camera permission not granted');
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBlueColor,
       body: SafeArea(
-        child: FutureBuilder<void>(
-          future: _cameraPermissionFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Camera permission is required to continue.'),
-                    ElevatedButton(
-                      onPressed: () => _retryPermission(),
-                      child: const Text('Request Permission'),
-                    ),
-                  ],
+        child: Stack(
+          children: [
+            FutureBuilder<CameraController?>(
+              future: cameraControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+                  if (snapshot.hasData && snapshot.data != null) {
+                    return Column(
+                      children: [
+                        buildLogo(),
+                        const StepIndicator(currentStep: 2, totalSteps: 3),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          height: MediaQuery.of(context).size.height * 0.5,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: kYellowColor, width: 4),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          child: CameraPreview(snapshot.data!),
+                        ),
+                      ],
+                    );
+                  } else {
+                    return const Center(child: Text('Camera not available'));
+                  }
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+            Positioned(
+              bottom: 16,
+              left: 16,
+              right: 16,
+              child: EaqoonsiButtonWidget(
+                onPressed: () async {
+                  try {
+                    final cameraController =
+                        await ref.read(cameraControllerProvider.future);
+                    if (cameraController != null) {
+                      final image = await cameraController.takePicture();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ImagePreviewScreen(
+                            fullName,
+                            email,
+                            password,
+                            imagePath: image.path,
+                          ),
+                        ),
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Camera not available')),
+                      );
+                    }
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Error: $e')),
+                    );
+                  }
+                },
+                text: 'Capture',
+                options: EaqoonsiButtonOptions(
+                  width: 130,
+                  height: 52,
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                  iconPadding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                  color: EAqoonsiTheme.of(context).primaryBackground,
+                  textStyle: EAqoonsiTheme.of(context).titleSmall.override(
+                        fontFamily: 'Plus Jakarta Sans',
+                        color: Colors.white,
+                        fontSize: 16,
+                        letterSpacing: 0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                  elevation: 3,
+                  borderSide: const BorderSide(
+                    color: Colors.transparent,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(40),
                 ),
-              );
-            } else {
-              return _buildCameraPreview();
-            }
-          },
+                showLoadingIndicator: true,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-
-  Widget _buildCameraPreview() {
-    final cameraControllerFuture = ref.watch(cameraControllerProvider.future);
-
-    return Stack(
-      children: [
-        FutureBuilder<CameraController?>(
-          future: cameraControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(child: Text('Error: ${snapshot.error}'));
-              }
-              if (snapshot.hasData && snapshot.data != null) {
-                return Column(
-                  children: [
-                    buildLogo(),
-                    const StepIndicator(currentStep: 2, totalSteps: 3),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: kYellowColor, width: 4),
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      child: CameraPreview(snapshot.data!),
-                    ),
-                  ],
-                );
-              } else {
-                return const Center(child: Text('Camera not available'));
-              }
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-        Positioned(
-          bottom: 16,
-          left: 16,
-          right: 16,
-          child: EaqoonsiButtonWidget(
-            onPressed: () => _captureImage(context),
-            text: 'Capture',
-            options: EaqoonsiButtonOptions(
-              width: 130,
-              height: 52,
-              padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-              iconPadding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-              color: EAqoonsiTheme.of(context).primaryBackground,
-              textStyle: EAqoonsiTheme.of(context).titleSmall.override(
-                    fontFamily: 'Plus Jakarta Sans',
-                    color: Colors.white,
-                    fontSize: 16,
-                    letterSpacing: 0,
-                    fontWeight: FontWeight.w500,
-                  ),
-              elevation: 3,
-              borderSide: const BorderSide(
-                color: Colors.transparent,
-                width: 1,
-              ),
-              borderRadius: BorderRadius.circular(40),
-            ),
-            showLoadingIndicator: true,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Future<void> _captureImage(BuildContext context) async {
-    try {
-      final cameraController = await ref.read(cameraControllerProvider.future);
-      if (cameraController != null) {
-        final image = await cameraController.takePicture();
-        if (mounted) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ImagePreviewScreen(
-                widget.fullName,
-                widget.email,
-                widget.password,
-                imagePath: image.path,
-              ),
-            ),
-          );
-        }
-      } else {
-        _showErrorSnackBar('Camera not available');
-      }
-    } catch (e) {
-      _showErrorSnackBar('Error: $e');
-    }
-  }
-
-  void _showErrorSnackBar(String message) {
-    if (mounted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(message)),
-      );
-    }
-  }
-
-  Future<void> _retryPermission() async {
-    setState(() {
-      _cameraPermissionFuture = _requestCameraPermission();
-    });
-  }
-
-  // @override
-  // void dispose() {
-  //   ref.read(cameraControllerProvider.notifier).dispose();
-  //   super.dispose();
-  // }
 }
 
 class ImagePreviewScreen extends ConsumerWidget {
